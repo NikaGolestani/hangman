7e963463376b006cf9c779132c0f0a50b43cb044
Nika Golestani <ng8224@gmail.com>
2022-12-04 18:12:56 +0300

    create version alfa

---
+++ hangman.py
@@ -0,0 +1,39 @@
+import random
+a=random.randint(0,147)
+b=random.randint(0,16)
+f = open("py\hangman\words4.txt", "r")
+c=f.readlines()[a]
+c=c[:84]
+d=c.split(" ")
+word=d[b]
+emptyword=["_ "]*len(word)
+guessed=[]
+i=0
+prints=["______\n |    | \n |     \n |   \n |    \n |    \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |    \n |    \n |    \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |   / \n |    \n |    \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |   /| \n |    \n |    \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |   /|\ \n |    \n |    \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |   /|\ \n |    | \n |    \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |   /|\ \n |    | \n |   /  \n_|_\n|   |______\n|          |\n|__________|",
+        "______\n |    | \n |    O \n |   /|\ \n |    | \n |   / \ \n_|_\n|   |______\n|          |\n|__________|"]
+while True:
+    print(prints[i])
+    if i ==7 or "_ " not in emptyword:
+        break
+    print("".join(emptyword)," | ",guessed)
+    guess=input("Type letter:")
+    if len(guess)==1 and guess not in guessed:
+        if guess in word:
+            for e in range(len(word)):
+                if word[e]==guess:
+                    emptyword[e]=guess
+        else:
+            guessed.append(guess)
+            i+=1
+if i<7:
+    print("You WON!!!")
+else:
+    print("Maybe next time:(")
+print("The word is",word.upper())
+    
